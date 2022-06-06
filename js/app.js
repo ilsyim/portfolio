7f4222d1321f0d9cca369a1d890417ef07858f72
ilysim <illymarie1@live.com>
2022-06-06 12:46:45 -0500

    add profile pic

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-console.log('hello')
+myProjects : [
+  {image:, title: "Tic-Tac-Toe", description: "Tic-tac-toe project", github:"https://github.com/ilsyim/ttt-weekend", deploy:"https://ttt-ilysim.netlify.app/"}
+]
+
+function welcomeMessage(){
+let guestName = prompt("Please enter your name")
+}
