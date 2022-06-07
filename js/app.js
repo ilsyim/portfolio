7ce551417c6e3f9ffa2c6859f0123b0a3dc4f7f9
ilysim <illymarie1@live.com>
2022-06-06 22:16:30 -0500

    set up flex/grid and link sections in nav

--- js/app.js
+++ js/app.js
@@ -2,12 +2,17 @@
 
 
 /*-------------------------------- Variables --------------------------------*/
-
+guestName = ""
 
 /*------------------------ Cached Element References ------------------------*/
 document.querySelector("#favicon")
-const guestMessage = document.querySelector(".message")
+const guestMessage = document.querySelector("#message")
 const me = document.querySelector("#me")
+const navList = document.querySelectorAll(".nav")
+const aboutBtn = document.querySelector("#about-button")
+const workBtn = document.querySelector("#work-button")
+const contactBtn = document.querySelector("#contact-button")
+const resumeBtn = document.querySelector("#resume-button")
 /*----------------------------- Event Listeners -----------------------------*/
 
 
@@ -20,11 +25,23 @@ const me = document.querySelector("#me")
 
 // }
 // let guestName = prompt("Please enter your name:")
-// guestMessage.push(`Welcome to my portfolio, ${ guestName}!`)  
+// guestMessage.push(`Welcome to my portfolio, ${ guestName}!`)
+// guestMessage.addEventListener("onclick", addName){
+//   guestMessage.innerHTML = ""
+//   function addName(){
+    
+
+//   }
+// }  
+aboutBtn.addEventListener("click", onClick) {
+  function onClick(){
+    document.querySelector("#about-button")
+  }
+}
 
 me.addEventListener("mouseenter", function(event){
-  event.target.style.color = "#f4b860" ; 
+  event.target.style.color = "#F4B860"; 
   setTimeout (function(){
     event.target.style.color = ""
-  }, 700)
+  }, 500)
 })
