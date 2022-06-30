1e9ee4149ed21a02efd901d6e99deea891c78df0
ilysim <illymarie1@live.com>
2022-06-29 21:32:53 -0500

    add bookcase to work

--- js/project-data.js
+++ js/project-data.js
@@ -5,6 +5,14 @@ let projectData = [
     image: '/assets/ttt.png',
     github: 'https://github.com/ilsyim/portfolio',
     description: 'My first project with GA, tic-tac-toe.'
+  },
+  {
+    title: 'bookCase',
+    deployment: 'https://bookcase-ilysim.herokuapp.com/books',
+    image:'/assets/bookCase2.png',
+    github:'https://github.com/ilsyim/bookcase',
+    description: 'An app designed for book lovers.'
+
   }
 ]
 export {
